ca7cfe2527006a182be97bcfd4f1f2344a6050bb
Sridharchollang <153913190+Sridharchollang@users.noreply.github.com>
2026-06-15 20:22:55 +0530

    Update Jenkins_AmazonLinux.sh for tmp directory setup
    Added commands to create and mount a temporary directory.

--- Jenkins_AmazonLinux.sh
+++ Jenkins_AmazonLinux.sh
@@ -1,16 +1,15 @@
 sudo yum update -y
-
 sudo yum install java-21-amazon-corretto -y
-
 java -version
-
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2026.key
-
 sudo yum install jenkins git -y
-
 sudo systemctl enable jenkins
-
-sudo systemctl start jenkins
+sudo mkdir -p /var/tmp_disk
+sudo chmod 1777 /var/tmp_disk
+sudo mount --bind /var/tmp_disk /tmp
+echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
+sudo systemctl mask tmp.mount
+df -h /tmp
+sudo systemctl restart jenkins
 sudo systemctl status jenkins
