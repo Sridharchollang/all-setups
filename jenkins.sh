c51f93a1d1ebe6c66f2e1e742eaeee6e8ee25e59
Sridharchollang <153913190+Sridharchollang@users.noreply.github.com>
2026-06-15 19:35:13 +0530

    Update Java installation to OpenJDK 21
    Replaced Java installation with OpenJDK 21 and updated alternatives.

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,10 @@ sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
-sudo yum install java-17-amazon-corretto -y
+sudo apt install -y openjdk-21-jdk
+sudo update-alternatives --set java /usr/lib/jvm/java-21-openjdk-amd64/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-21-openjdk-amd64/bin/javac
+java -version
 sudo yum install jenkins git -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
@@ -14,3 +17,4 @@ echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
 sudo systemctl mask tmp.mount
 df -h /tmp
 sudo systemctl restart jenkins
+sudo systemctl status jenkins
