31ba31e0083b77d5925947fd6fd2ef3d298fe507
Sridharchollang <153913190+Sridharchollang@users.noreply.github.com>
2026-06-16 23:39:34 +0530

    Setup /var/tmp_disk mount for Jenkins
    Added commands to create and mount /var/tmp_disk to /tmp.

--- ubuntu-jenkins.sh
+++ ubuntu-jenkins.sh
@@ -23,8 +23,16 @@ sudo systemctl enable jenkins
 
 sudo systemctl start jenkins
 
+sudo mkdir -p /var/tmp_disk
+sudo chmod 1777 /var/tmp_disk
+sudo mount --bind /var/tmp_disk /tmp
+echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
+sudo systemctl mask tmp.mount
+df -h /tmp
+sudo systemctl restart jenkins
 sudo systemctl status jenkins
 
+sudo systemctl status jenkins
 jenkins --version
 
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
