4de08a7c4d44c020d07a81abdb6c4788fc732f3e
Sridharchollang <153913190+Sridharchollang@users.noreply.github.com>
2026-06-26 17:21:45 +0530

    Modify sonar.sh for Jenkins job and comments
    Updated Jenkins job configuration and added comments for clarity.

--- sonar.sh
+++ sonar.sh
@@ -18,11 +18,13 @@ su - sonar -c "sh /opt/sonarqube-8.9.6.50800/bin/linux-x86-64/sonar.sh start"
 #echo "user=admin & password=admin"
 
 
-Jenkins
+
+## Jenkins job ==>> Build Steps ==>>  Invoke top-level Maven targets==>> Maven Version ==>> maven ==>> Goals
+
 ##clean  package  sonar:sonar  -Dsonar.projectKey=myweb  -Dsonar.projectName=Java-Home-MyWeb  -Dsonar.sources=src/main/webapp  -Dsonar.host.url=http://54.234.54.99:9000  -Dsonar.login=8167c1ac5fb92a9c3a2ae2b79360862fb04223d4
 
 
-#Tool configuration
+#Tool configuration in manage jenkins
 #SonarQube Scanner installations
 #Maven installations
 
